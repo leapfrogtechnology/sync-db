9b010b309cb1aafa3cc758a3d69cf9f13d0e344d
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-06-29 21:05:07 +0545

    Add fs util

---
+++ src/util/fs.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs';
+
+/**
+ * Read file contents.
+ *
+ * @param {string} filename
+ * @returns {Promise<string>}
+ */
+export function read(filename: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        reject(err);
+
+        return;
+      }
+
+      resolve(data.toString());
+    });
+  });
+}
+
+/**
+ * Check if the file exists.
+ *
+ * @param {string} filename
+ * @returns {Promise<boolean>}
+ */
+export function exists(filename: string): Promise<boolean> {
+  return new Promise(resolve => {
+    fs.access(filename, fs.constants.F_OK, err => resolve(!!err));
+  });
+}
+
+/**
+ * Read all files in a directory.
+ *
+ * @param {string} pathName
+ * @returns {Promise<string[]>}
+ */
+export function glob(pathName: string): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    fs.readdir(pathName, (err, data) => {
+      if (err) {
+        reject(err);
+
+        return;
+      }
+
+      resolve(data);
+    });
+  });
+}
