fb98a4c8a472857e670612ab5f500b91a28f43a7
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-07-05 21:54:28 +0545

    Fix sql runner tests

--- test/sqlRunner.test.ts
+++ test/sqlRunner.test.ts
@@ -5,38 +5,38 @@ import * as sqlRunner from '../src/sqlRunner';
 
 describe('UTIL: sqlRunner', () => {
   describe('extractSqlFileInfo()', () => {
-    it('should return the parsed sql file info from the path (type = functions)', () => {
-      expect(sqlRunner.extractSqlFileInfo('functions/test_data/test_function_name.sql')).to.deep.equal({
+    it('should return the parsed sql file info from the path (type = function)', () => {
+      expect(sqlRunner.extractSqlFileInfo('function/test_data/test_function_name.sql')).to.deep.equal({
         name: 'test_function_name',
         fqon: 'test_data.test_function_name',
-        type: 'functions',
+        type: 'function',
         schema: 'test_data'
       });
     });
 
-    it('should return the parsed sql file info from the path (type = procedures)', () => {
-      expect(sqlRunner.extractSqlFileInfo('procedures/test_data/test_procedure_name.sql')).to.deep.equal({
+    it('should return the parsed sql file info from the path (type = procedure)', () => {
+      expect(sqlRunner.extractSqlFileInfo('procedure/test_data/test_procedure_name.sql')).to.deep.equal({
         name: 'test_procedure_name',
         fqon: 'test_data.test_procedure_name',
-        type: 'procedures',
+        type: 'procedure',
         schema: 'test_data'
       });
     });
 
-    it('should return the parsed sql file info from the path (type = views)', () => {
-      expect(sqlRunner.extractSqlFileInfo('views/test_data/test_view_name.sql')).to.deep.equal({
+    it('should return the parsed sql file info from the path (type = view)', () => {
+      expect(sqlRunner.extractSqlFileInfo('view/test_data/test_view_name.sql')).to.deep.equal({
         name: 'test_view_name',
         fqon: 'test_data.test_view_name',
-        type: 'views',
+        type: 'view',
         schema: 'test_data'
       });
     });
 
-    it('should return the parsed sql file info from the path (type = schemas)', () => {
-      expect(sqlRunner.extractSqlFileInfo('schemas/test_data.sql')).to.deep.equal({
+    it('should return the parsed sql file info from the path (type = schema)', () => {
+      expect(sqlRunner.extractSqlFileInfo('schema/test_data.sql')).to.deep.equal({
         name: 'test_data',
         fqon: 'test_data',
-        type: 'schemas',
+        type: 'schema',
         schema: undefined
       });
     });
@@ -44,25 +44,25 @@ describe('UTIL: sqlRunner', () => {
 
   describe('getDropStatement()', () => {
     it('should return DROP statement for a function', () => {
-      expect(sqlRunner.getDropStatement('functions', 'test.hello_world')).to.equal(
+      expect(sqlRunner.getDropStatement('function', 'test.hello_world')).to.equal(
         'DROP FUNCTION IF EXISTS test.hello_world'
       );
     });
 
     it('should return DROP statement for a procedure', () => {
-      expect(sqlRunner.getDropStatement('procedures', 'test.hello_world')).to.equal(
+      expect(sqlRunner.getDropStatement('procedure', 'test.hello_world')).to.equal(
         'DROP PROCEDURE IF EXISTS test.hello_world'
       );
     });
 
     it('should return DROP statement for a schema', () => {
-      expect(sqlRunner.getDropStatement('schemas', 'test.hello_world')).to.equal(
+      expect(sqlRunner.getDropStatement('schema', 'test.hello_world')).to.equal(
         'DROP SCHEMA IF EXISTS test.hello_world'
       );
     });
 
     it('should return DROP statement for a view', () => {
-      expect(sqlRunner.getDropStatement('views', 'test.hello_world')).to.equal('DROP VIEW IF EXISTS test.hello_world');
+      expect(sqlRunner.getDropStatement('view', 'test.hello_world')).to.equal('DROP VIEW IF EXISTS test.hello_world');
     });
   });
 });
