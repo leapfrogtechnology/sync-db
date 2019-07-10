0b4647790d34504ac1b0004cd0735712f29fb0ad
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-07-10 11:12:59 +0545

    Simplify promise rejection

--- src/util/fs.ts
+++ src/util/fs.ts
@@ -10,9 +10,7 @@ export function read(filename: string): Promise<string> {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, (err, data) => {
       if (err) {
-        reject(err);
-
-        return;
+        return reject(err);
       }
 
       resolve(data.toString());
@@ -42,9 +40,7 @@ export function glob(pathName: string): Promise<string[]> {
   return new Promise((resolve, reject) => {
     fs.readdir(pathName, (err, data) => {
       if (err) {
-        reject(err);
-
-        return;
+        return reject(err);
       }
 
       resolve(data);
