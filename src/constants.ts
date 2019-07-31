2f8544769c74f502f339257aeaad3fbb4188176b
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-07-31 14:07:02 +0545

    Add basePath in the config

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,14 @@
 /* Global Constants */
 
+import * as path from 'path';
+
 import Configuration from './domain/Configuration';
 
 export const CONFIG_FILENAME = 'sync-db.yml';
 export const CONNECTIONS_FILENAME = 'connections.sync-db.json';
 
 export const DEFAULT_CONFIG: Configuration = {
+  basePath: path.resolve(process.cwd() + 'src/sql/'),
   sql: [],
   hooks: {
     pre_sync: [],
