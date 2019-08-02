aa1a403932efa72908cfec86162beb2fdc41711a
Shraday Shakya <shradayshakya@gmail.com>
2019-08-02 14:41:46 +0545

    Fix formatting

--- examples/node-app-mssql/README.md
+++ examples/node-app-mssql/README.md
@@ -1,6 +1,6 @@
 # Node MSSQL Example (JavaScript)
 
-Sample project with MSSQL.
+Sample project for Microsoft SQL Server.
 
 ## Running
 
@@ -10,7 +10,15 @@ Install dependencies.
 $ yarn
 ```
 
-Sync databases.
+Configure database connection(s).
+
+```bash
+$ cp connections.sync-db.json.example connections.sync-db.json
+```
+
+Edit your `connections.sync-db.json` with the db creds.
+
+Synchronize all database objects (views, functions, procedures, schemas, etc)
 
 ```
 $ yarn sync
@@ -26,13 +34,16 @@ $ yarn start
 
 ```
 List of table names in the database.
-[ { name: 'table1' },
-  { name: 'table2' },
-  { name: 'table3' } ]
+[ 'table1',
+  'table2',
+  'table3' ]
 List of user names in the database.
-[ { name: 'user1' },
-  { name: 'user2' },
-  { name: 'user3' } ]
-Product of 6 and 7 is 42
-Current date time is  2019-08-02T08:18:10.010Z
+[ 'user1',
+  'user2',
+  'user3' ]
+Calculations: 
+ { 'Sum of 6 and 7 is': 13,
+  'Product of 6 and 7 is': 42,
+  'Square of 6 is': 36 }
+Current date time is  2019-08-02T08:50:59.430Z
 ```
--- examples/node-app-mssql/src/index.js
+++ examples/node-app-mssql/src/index.js
@@ -15,14 +15,17 @@ const connections = require('../connections.sync-db.json');
     const tables = await db.raw('SELECT * FROM utils.vw_table_names');
     const users = await db.raw('SELECT * FROM utils.vw_user_names');
     const [{ result: product }] = await db.raw('SELECT utils.product(6, 7) AS result;');
+    const [{ result: sum }] = await db.raw('SELECT dbo.sum(6, 7) AS result;');
+    const [{ result: square }] = await db.raw('SELECT dbo.square(6) AS result;');
     const [{ result: date }] = await db.raw('EXEC utils.get_date;');
 
-    console.log('List of table names in the database.', tables);
-
-    console.log('List of user names in the database.', users);
-
-    console.log('Product of 6 and 7 is', product);
-
+    console.log('List of table names in the database:\n', tables.map(({ name }) => name));
+    console.log('List of user names in the database.\n', users.map(({ name }) => name));
+    console.log('Calculations:\n', {
+      'Sum of 6 and 7 is': sum,
+      'Product of 6 and 7 is': product,
+      'Square of 6 is': square
+    });
     console.log('Current date time is ', date);
 
     process.exit(0);
