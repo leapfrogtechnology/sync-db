96ce1312ef13e2f48596af163bb8c423b21cd3d9
Safal Pandey <safal.pandey.sp@gmail.com>
2019-08-20 16:13:48 +0545

    Create util to get config from a connection instance

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,17 +1,32 @@
 import * as Knex from 'knex';
 
 import ConnectionConfig from '../domain/ConnectionConfig';
+import ConnectionInstance from '../domain/ConnectionInstance';
 
 /**
- * Creates a knex database connection instance from
+ * Creates a connection instance from
  * the provided database configuration.
  *
- * @param {Knex.Config} connectionConfig
- * @returns {Knex}
+ * @param {ConnectionConfig} connectionConfig
+ * @returns {ConnectionInstance}
  */
-export function createInstance(connectionConfig: ConnectionConfig): Knex {
+export function createInstance(connectionConfig: ConnectionConfig): ConnectionInstance {
   return Knex({
     client: connectionConfig.client,
     connection: connectionConfig
   });
+}
+
+/**
+ * Reteives connection configurations from a connection instance.
+ *
+ * @param {ConnectionInstance} db
+ * @returns {ConnectionConfig}
+ */
+export function getConfig(db: ConnectionInstance): ConnectionConfig {
+  return {
+    ...db.client.config.connection,
+    id: db.client.config.connection.database,
+    client: db.client.config.client
+  };
 }
