fd7f447d9608f11fdccf32ea12a8921fe9eee470
Safal Pandey <safal.pandey.sp@gmail.com>
2019-08-26 14:23:27 +0545

    Use public method from Connection to get config in logger

--- src/logger.ts
+++ src/logger.ts
@@ -4,4 +4,4 @@ import Connection from './Connection';
 
 export const SYNC_DB = 'sync-db';
 export const log = debug(SYNC_DB);
-export const dbLogger = (conn: Connection) => log.extend(conn.config.id || conn.config.database);
+export const dbLogger = (conn: Connection) => log.extend(conn.getConfig().id || conn.getConfig().database);
