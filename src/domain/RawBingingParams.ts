dc993119a2fc4fc267c142cb3e5d8a8e0cbe0990
Safal Pandey <safal.pandey.sp@gmail.com>
2019-08-26 14:16:11 +0545

    Add type and interface for RawBindingParams and ValueMap

---
+++ src/domain/RawBingingParams.ts
@@ -0,0 +1,22 @@
+import * as Knex from 'knex';
+
+type Value = Date | string | number | boolean | Date[] | string[] | number[] | boolean[] | Buffer | Knex.Raw;
+
+type RawBindingParams = (
+  | Date
+  | Buffer
+  | string
+  | number
+  | Date[]
+  | boolean
+  | Knex.Raw
+  | string[]
+  | number[]
+  | boolean[]
+  | Knex.QueryBuilder)[];
+
+export interface ValueMap {
+  [key: string]: Value | Knex.QueryBuilder;
+}
+
+export default RawBindingParams;
