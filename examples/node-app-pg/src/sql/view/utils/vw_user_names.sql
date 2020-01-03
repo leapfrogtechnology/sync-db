7e3ecd8e1e48d43a26a34f60e6708440795037f7
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-01-03 14:08:00 +0545

    Update vw_user_names.sql

--- examples/node-app-pg/src/sql/view/utils/vw_user_names.sql
+++ examples/node-app-pg/src/sql/view/utils/vw_user_names.sql
@@ -3,6 +3,7 @@
 -- View to fetch server login names.
 --
 CREATE VIEW [utils].vw_user_names
-AS
-  (SELECT name
-  FROM sys.server_principals);
+AS (
+  SELECT name
+  FROM sys.server_principals
+);
