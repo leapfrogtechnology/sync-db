ee4fd0e7859be43cd6df79b23125dd855d52d8ef
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-01-19 19:19:46 +0545

    Move expandEnvVars to env.ts

--- src/services/configInjection.ts
+++ src/services/configInjection.ts
@@ -1,7 +1,5 @@
 import Mapping from '../domain/Mapping';
 
-const REGEX_VAR_EXPANSION = /\$\{([a-zA-Z]\w+)\}/g;
-
 /**
  * Update variables to be injected and expand
  * environment variables to be expanded.
@@ -17,21 +15,4 @@ export function updateInjectedConfigVars(vars: Mapping<string>): Mapping<string>
   );
 
   return result;
-}
-
-/**
- * Expands the environment variables present in the string.
- * Returns the string as is, if no variables were present. And,
- * in case the variable used in the string isn't defined it is
- * substituted with an empty string ('').
- *
- *  Example:
- *    Input: 'Current user is ${USER} at ${HOST}.'
- *    Output: 'Current user is kabir at leapfrog.'
- *
- * @param {string} value
- * @returns {string}
- */
-export function expandEnvVar(value: string): string {
-  return value.replace(REGEX_VAR_EXPANSION, (_, key) => process.env[key] || '');
 }
