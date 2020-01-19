03365672bc8f1b7b61a794da62b11f24f5699a02
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-01-19 19:25:11 +0545

    Add failing test for injected config vars

--- test/configInjection.test.ts
+++ test/configInjection.test.ts
@@ -13,5 +13,21 @@ describe('Services: configInjection', () => {
       };
       expect(updateInjectedConfigVars(vars)).to.deep.equal(vars);
     });
+
+    it('should expand all the environment variables found in the values', () => {
+      const vars = {
+        var1: 'Foo',
+        var2: 'Bar',
+        var3: '${TEST_ENV_VALUE1}',
+        var4: '${TEST_ENV_VALUE2}'
+      };
+
+      expect(updateInjectedConfigVars(vars)).to.deep.equal({
+        var1: 'Foo',
+        var2: 'Bar',
+        var3: 'Test value 1',
+        var4: 'Test value 2'
+      });
+    });
   });
 });
