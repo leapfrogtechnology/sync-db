2487817052cb38f5f2b610921372f4cd44766374
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-01-20 23:32:11 +0545

    Generate tasks data

--- examples/node-app-mssql/src/sql/procedure/testdata/setup_data.sql
+++ examples/node-app-mssql/src/sql/procedure/testdata/setup_data.sql
@@ -32,4 +32,40 @@ BEGIN
     INSERT INTO dbo.users (email, password)
     VALUES (@admin_email, CONVERT(VARCHAR(100), REPLACE(NEWID(), '-', '')));
 
+  -- Empty tasks data.
+  DELETE FROM tasks;
+
+  -- Add tasks
+  INSERT INTO dbo.tasks
+    (user_id, title, description, is_complete)
+  SELECT
+    u.id as user_id,
+    t.title,
+    t.description,
+    t.is_complete
+  FROM (
+    VALUES
+      ('user1@example.com', 'Task 1', 'This is task 1.', 1),
+      ('user1@example.com', 'Task 2', 'This is task 2.', 0),
+      ('user1@example.com', 'Task 3', 'This is task 3.', 0),
+      ('user1@example.com', 'Task 4', 'This is task 4.', 0),
+      ('user1@example.com', 'Task 5', 'This is task 5.', 1),
+      ('user2@example.com', 'Task 6', 'This is task 6.', 0),
+      ('user2@example.com', 'Task 7', 'This is task 7.', 1),
+      ('user2@example.com', 'Task 8', 'This is task 8.', 0),
+      ('user2@example.com', 'Task 9', 'This is task 9.', 0),
+      ('user2@example.com', 'Task 10', 'This is task 10.', 1),
+      ('user3@example.com', 'Task 11', 'This is task 11.', 0),
+      ('user3@example.com', 'Task 12', 'This is task 12.', 0),
+      ('user3@example.com', 'Task 13', 'This is task 13.', 1),
+      ('user3@example.com', 'Task 14', 'This is task 14.', 0),
+      ('user3@example.com', 'Task 15', 'This is task 15.', 1),
+      ('user4@example.com', 'Task 16', 'This is task 16.', 0),
+      ('user4@example.com', 'Task 17', 'This is task 17.', 1),
+      ('user4@example.com', 'Task 18', 'This is task 18.', 1),
+      ('user4@example.com', 'Task 19', 'This is task 19.', 1),
+      ('user4@example.com', 'Task 20', 'This is task 20.', 0)
+  ) AS t(user_email, title, description, is_complete)
+  INNER JOIN dbo.users u ON u.email = t.user_email
+
 END
