bbb95a31d5c17262a0e435d1c0cd4d6359bc1cd9
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-02-04 02:05:34 +0545

    Add db.getConfig() function

--- src/util/db.ts
+++ src/util/db.ts
@@ -1,4 +1,6 @@
 import * as Knex from 'knex';
+import { getConnectionId } from '../config';
+import ConnectionConfig from '../domain/ConnectionConfig';
 
 /**
  * Returns true if the provided object is a knex connection instance.
@@ -10,4 +12,19 @@ import * as Knex from 'knex';
  */
 export function isKnexInstance(obj: any): obj is Knex {
   return !!(obj.prototype && obj.prototype.constructor && obj.prototype.constructor.name === 'knex');
+}
+
+/**
+ * Extracts the connection config params
+ * using provided Knex connection instance.
+ *
+ * @param {Knex} db
+ * @returns {Connection}
+ */
+export function getConfig(db: Knex): ConnectionConfig {
+  return {
+    ...db.client.config.connection,
+    client: db.client.config.client,
+    id: getConnectionId(db.client.config.connection)
+  };
 }
