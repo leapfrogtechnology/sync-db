9be069abd8b304f79c9c1ceb341e03b42730ad12
silwalanish <anishsilwal@lftechnology.com>
2020-02-03 20:31:23 +0545

    Cast DB_PORT to number when generating connection

--- src/config.ts
+++ src/config.ts
@@ -80,7 +80,7 @@ export function resolveConnectionsFromEnv(): ConnectionConfig[] {
   const connection = {
     client: process.env.DB_CLIENT,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: +process.env.DB_PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
