b3717b3abbf38fb23c5b92b2cf5537788b9ec81b
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-02-03 23:33:16 +0545

    Use version 1.0.0-experimental.9

--- examples/node-app-mssql/src/sql/scripts/setup.sql
+++ examples/node-app-mssql/src/sql/scripts/setup.sql
@@ -5,9 +5,9 @@ DECLARE @create_admin BIT;
 DECLARE @admin_email VARCHAR(100);
 DECLARE @task_status VARCHAR(100);
 
-SET @create_admin = (SELECT IIF([value] = 'true', 1, 0) FROM __injected_config WHERE [key] = 'create_admin');
-SET @admin_email = (SELECT [value] FROM __injected_config WHERE [key] = 'admin_email');
-SET @task_status = (SELECT [value] FROM __injected_config WHERE [key] = 'task_status');
+SET @create_admin = (SELECT IIF([value] = 'true', 1, 0) FROM __sync_db_injected_config WHERE [key] = 'create_admin');
+SET @admin_email = (SELECT [value] FROM __sync_db_injected_config WHERE [key] = 'admin_email');
+SET @task_status = (SELECT [value] FROM __sync_db_injected_config WHERE [key] = 'task_status');
 
 -- Setup data.
 EXEC testdata.setup_data @create_admin, @admin_email, @task_status;
