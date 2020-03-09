f2bb1ef4f94a732851fbcfa2c4a8f32d8d82bf88
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-10 01:03:42 +0545

    Add interface SqlMigrationEntry

---
+++ src/domain/SqlMigrationEntry.ts
@@ -0,0 +1,11 @@
+import SqlCode from './SqlCode';
+
+interface SqlMigrationEntry {
+  name: string;
+  queries: {
+    up?: SqlCode;
+    down?: SqlCode;
+  };
+}
+
+export default SqlMigrationEntry;
