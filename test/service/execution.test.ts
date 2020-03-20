85f7492bb5e75184a8ff98c48f08a9bcc1c85653
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-21 02:45:41 +0545

    Add tests for sequential and parallel execution

---
+++ test/service/execution.test.ts
@@ -0,0 +1,53 @@
+import { expect } from 'chai';
+import { it, describe } from 'mocha';
+
+import { timeout } from '../../src/util/promise';
+import SyncConfig from '../../src/domain/SyncConfig';
+import * as executionService from '../../src/services/execution';
+
+const getProcesses = (tracker: string[]) => [
+  () =>
+    timeout(45)
+      .then(() => tracker.push('Task A'))
+      .then(() => 'Task A'),
+  () =>
+    timeout(50)
+      .then(() => tracker.push('Task B'))
+      .then(() => 'Task B'),
+  () =>
+    timeout(39)
+      .then(() => tracker.push('Task C'))
+      .then(() => 'Task C'),
+  () =>
+    timeout(40)
+      .then(() => tracker.push('Task D'))
+      .then(() => 'Task D')
+];
+
+describe('Services: execution', () => {
+  describe('executeProcesses', () => {
+    it('should be able to run things with sequentially [strategy = sequential].', async () => {
+      const tracker: string[] = [];
+      const processes = getProcesses(tracker);
+
+      const result = await executionService.executeProcesses(processes, {
+        executionStrategy: 'sequential'
+      } as SyncConfig);
+
+      expect(result).to.deep.equal(['Task A', 'Task B', 'Task C', 'Task D']);
+      expect(tracker).to.deep.equal(['Task A', 'Task B', 'Task C', 'Task D']);
+    });
+
+    it('should be able to run things in-parallel [strategy = parallel].', async () => {
+      const tracker: string[] = [];
+      const processes = getProcesses(tracker);
+
+      const result = await executionService.executeProcesses(processes, {
+        executionStrategy: 'parallel'
+      } as SyncConfig);
+
+      expect(result).to.deep.equal(['Task A', 'Task B', 'Task C', 'Task D']);
+      expect(tracker).to.deep.equal(['Task C', 'Task D', 'Task A', 'Task B']);
+    });
+  });
+});
