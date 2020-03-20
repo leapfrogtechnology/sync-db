1f058778bb55895bce9ee39c55e53ab22e5db6c2
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-21 01:13:04 +0545

    Update tests

--- test/promise.test.ts
+++ test/promise.test.ts
@@ -1,6 +1,6 @@
-import { it, describe } from 'mocha';
 import { expect } from 'chai';
 import { promisify } from 'util';
+import { it, describe } from 'mocha';
 
 import * as promise from '../src/util/promise';
 
@@ -45,18 +45,12 @@ describe('UTIL: promise', () => {
 
   describe('runSequentially', () => {
     it('should resolve a promise containing the results of the promises in the same order.', async () => {
-      const promises = [
-        () => timeout(100).then(() => 1),
-        () => timeout(80).then(() => 2),
-        () => timeout(60).then(() => 3),
-        () => timeout(40).then(() => 4),
-        () => timeout(20).then(() => 5),
-        () => timeout(0).then(() => 6)
-      ];
+      const source = [{ value: 'one' }, { value: 'two' }, { value: 'three' }, { value: 'four' }, { value: 'five' }];
+      const promisers = source.map(({ value }) => () => Promise.resolve(value));
 
-      const result = await promise.runSequentially(promises);
+      const result = await promise.runSequentially(promisers);
 
-      expect(result).to.deep.equal([1, 2, 3, 4, 5, 6]);
+      expect(result).to.deep.equal(['one', 'two', 'three', 'four', 'five']);
     });
   });
 });
