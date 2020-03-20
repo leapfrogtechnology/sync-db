3792c6ed9f06e23817a442df92c6ea23c118fe51
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-21 00:35:51 +0545

    Add a failing test for runSequentially

--- test/promise.test.ts
+++ test/promise.test.ts
@@ -7,6 +7,23 @@ import * as promise from '../src/util/promise';
 const timeout = promisify(setTimeout);
 
 describe('UTIL: promise', () => {
+  it('should run promises sequentially.', async () => {
+    const promiseResolutionTracker: number[] = [];
+
+    const promises = [
+      timeout(100).then(() => promiseResolutionTracker.push(1)),
+      timeout(80).then(() => promiseResolutionTracker.push(2)),
+      timeout(60).then(() => promiseResolutionTracker.push(3)),
+      timeout(40).then(() => promiseResolutionTracker.push(4)),
+      timeout(20).then(() => promiseResolutionTracker.push(5)),
+      timeout(0).then(() => promiseResolutionTracker.push(6))
+    ];
+
+    await promise.runSequentially(promises);
+
+    expect(promiseResolutionTracker).to.deep.equal([1, 2, 3, 4, 5, 6]);
+  });
+
   describe('runSequentially', () => {
     it('should resolve a promise containing the results of the promises in the same order.', async () => {
       const promises = [
