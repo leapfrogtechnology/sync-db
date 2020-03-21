87577875ad45545a7cea53b0f8a1f98357841078
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-21 11:26:12 +0545

    Support fixed decimal number

--- src/util/misc.ts
+++ src/util/misc.ts
@@ -4,11 +4,16 @@ import { NS_PER_SEC } from '../constants';
  * Calculate elapsed time from the given start time (process.hrtime()).
  *
  * @param {[number, number]} timeStart
- * @returns {number}
+ * @param {(false | number)} fixed
+ * @returns {(number | string)}
  */
-export function getElapsedTime(timeStart: [number, number]): number {
+export function getElapsedTime(timeStart: [number, number], fixed: false | number = 2): number {
   const timeDiff = process.hrtime(timeStart);
   const timeElapsed = Number(timeDiff[0]) + Number(timeDiff[1] / NS_PER_SEC);
 
-  return timeElapsed;
+  if (fixed === false) {
+    return timeElapsed;
+  }
+
+  return Number(timeElapsed.toFixed(fixed));
 }
