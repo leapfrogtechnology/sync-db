a08577a4c6f237355f6ca2b24b3789a5d4f27c24
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-22 14:28:10 +0545

    Revamp KnexMigrationSource

---
+++ src/migration/KnexMigrationSource.ts
@@ -0,0 +1,60 @@
+import { dbLogger } from '../util/logger';
+import MigrationContext from '../domain/MigrationContext';
+
+/**
+ * MigrationSource class for the Knex Migration API.
+ */
+class KnexMigrationSource {
+  private migrationContext: MigrationContext;
+  private log: debug.Debugger;
+
+  /**
+   * KnexMigrationSource constructor.
+   *
+   * @param {MigrationEntry[]} migrationLists
+   */
+  constructor(migrationContext: MigrationContext) {
+    this.log = dbLogger(migrationContext.connectionId);
+    this.migrationContext = migrationContext;
+  }
+
+  /**
+   * Gets a list of migration names.
+   *
+   * @returns {Promise<string[]>}
+   */
+  getMigrations(): Promise<string[]> {
+    const migrations = this.migrationContext.keys();
+
+    this.log('getMigrations - resolve: %o', migrations);
+
+    return Promise.resolve(migrations);
+  }
+
+  /**
+   * Gets the name of the migration.
+   *
+   * @param {string} migration
+   * @returns {string}
+   */
+  getMigrationName(migration: string): string {
+    this.log('getMigrationName - resolve: ', migration);
+
+    return migration;
+  }
+  /**
+   * Get the migration runner.
+   *
+   * @param {SqlMigrationEntry} migration
+   */
+  getMigration(name: string) {
+    const migration = this.migrationContext.get(name);
+
+    this.log(`getMigration - ${name}`);
+    this.log(`getMigration - resolve: %o`, migration);
+
+    return migration;
+  }
+}
+
+export default KnexMigrationSource;
