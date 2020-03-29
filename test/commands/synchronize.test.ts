d07aa8651715628f99e113e5ed933c9551c25730
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-03-29 15:23:16 +0545

    Test synchronize command

--- test/commands/synchronize.test.ts
+++ test/commands/synchronize.test.ts
@@ -1,21 +1,17 @@
-import * as path from 'path';
 import { expect } from 'chai';
 import { it, describe } from 'mocha';
 
 import { runCli } from './util';
-import { mkdtemp, read } from '../../src/util/fs';
+import { mkdtemp } from '../../src/util/fs';
 
 describe('CLI: synchronize', () => {
-  describe('--version', () => {
-    it('should return the CLI version.', async () => {
+  describe('--help', () => {
+    it('should print help message.', async () => {
       const cwd = await mkdtemp();
-      const packageJson = await read(path.join(__dirname, '../../package.json'));
-      const { version } = JSON.parse(packageJson);
+      const { stdout } = await runCli(['synchronize', '--help'], { cwd });
 
-      const { stdout } = await runCli(['--version'], { cwd });
-
-      expect(stdout).contains('@leapfrogtechnology/sync-db');
-      expect(stdout).contains(version);
+      expect(stdout).contains('Synchronize database');
+      expect(stdout).contains(`USAGE\n  $ sync-db synchronize`);
     });
   });
 });
