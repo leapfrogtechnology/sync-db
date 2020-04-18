d672e3fc3ce3e9ba6c10cf399edb48c45b509b88
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-04-18 16:31:06 +0545

    Print colored info & errors

--- src/util/io.ts
+++ src/util/io.ts
@@ -1,3 +1,5 @@
+import { green, red } from 'chalk';
+
 /**
  * Prints a line into the console (stdout).
  *
@@ -6,6 +8,16 @@
  */
 export function printLine(message: string = ''): Promise<any> {
   return print(message.toString() + '\n');
+}
+
+/**
+ * Print an info line on the console (in green color).
+ *
+ * @param {string} [message='']
+ * @returns {Promise<any>}
+ */
+export function printInfo(message: string = ''): Promise<any> {
+  return printLine(green(message));
 }
 
 /**
@@ -26,5 +38,5 @@ export function print(message: string): Promise<any> {
  * @returns {Promise<any>}
  */
 export function printError(error: Error): Promise<any> {
-  return new Promise(resolve => process.stderr.write((error.stack || error).toString() + '\n', resolve));
+  return new Promise(resolve => process.stderr.write(red((error.stack || error).toString()) + '\n', resolve));
 }
