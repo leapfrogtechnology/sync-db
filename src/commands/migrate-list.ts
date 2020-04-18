797887c61ac06f067b4c5103f62089b096efabd9
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-04-18 16:10:09 +0545

    Build UX for the migrate-list command

--- src/commands/migrate-list.ts
+++ src/commands/migrate-list.ts
@@ -3,9 +3,7 @@ import { Command } from '@oclif/command';
 import { printLine } from '../util/io';
 import { loadConfig, resolveConnections } from '..';
 import { log } from '../util/logger';
-import { getElapsedTime } from '../util/ts';
-import SyncParams from '../domain/SyncParams';
-import ExecutionContext from '../domain/ExecutionContext';
+import { MigrationListParams, MigrationListResult } from '../service/migrator';
 
 /**
  * Migration command handler.
@@ -13,22 +11,43 @@ import ExecutionContext from '../domain/ExecutionContext';
 class MigrateList extends Command {
   static description = 'List migrations.';
 
-  /**
-   * Default CLI options for running synchronize.
-   *
-   * @param {*} userParams
-   * @returns {SyncParams}
-   */
-  getSyncParams(userParams: any): SyncParams {
+  getParams(): MigrationListParams {
     return {
-      ...userParams,
-      // Individual success handler
-      onSuccess: (context: ExecutionContext) =>
-        printLine(`  [✓] ${context.connectionId} - Successful (${context.timeElapsed}s)`),
+      onSuccess: async (result: MigrationListResult) => {
+        // ➜
+        await printLine(` ▸ ${result.connectionId}`);
 
-      // Individual error handler
-      onFailed: (context: ExecutionContext) =>
-        printLine(`  [✖] ${context.connectionId} - Failed (${context.timeElapsed}s)`)
+        log(result.data);
+
+        const [list1, list2] = result.data;
+        const ranCount = list1.length;
+        const remainingCount = list2.length;
+
+        // Completed migrations.
+        for (const item of list1) {
+          await printLine(`   • ${item}`);
+        }
+
+        // Remaining Migrations
+        for (const item of list2) {
+          await printLine(`   - ${item}`);
+        }
+
+        if (ranCount === 0 && remainingCount === 0) {
+          await printLine('   No migrations.');
+        } else if (remainingCount === 0) {
+          await printLine(`\n   All up to date.`);
+        } else {
+          await printLine(`\n   ${list2.length} migrations yet to be run.`);
+        }
+
+        await printLine();
+      },
+      onFailed: async (result: MigrationListResult) => {
+        printLine(` ▸ ${result.connectionId} - Failed (${result.timeElapsed}s)`);
+
+        await printLine(`   ${result.error}\n`);
+      }
     };
   }
 
@@ -38,34 +57,22 @@ class MigrateList extends Command {
    * @returns {Promise<void>}
    */
   async run(): Promise<void> {
-    const { flags: parsedFlags } = this.parse(MigrateList);
-    const params = this.getSyncParams({ ...parsedFlags });
+    const params = this.getParams();
 
     const config = await loadConfig();
     const connections = await resolveConnections();
     const { migrateList } = await import('../api');
-    const timeStart = process.hrtime();
 
     const results = await migrateList(config, connections, params);
 
     log('Results:', results);
-    console.log('Results', results); // tslint:disable-line
 
-    const successfulCount = results.filter(item => item.success).length;
-
-    if (successfulCount > 0) {
-      // Display output.
-      await printLine(
-        `List complete for ${successfulCount} / ${results.length} connection(s). ` + `(${getElapsedTime(timeStart)}s)`
-      );
-    }
+    const failedCount = results.filter(({ success }) => !success).length;
 
     // If all completed successfully, exit gracefully.
-    if (results.length === successfulCount) {
-      return process.exit(0);
-    }
+    const exitCode = failedCount === 0 ? 0 : -1;
 
-    throw new Error(`Failed to get the list for some connections.`);
+    process.exit(exitCode);
   }
 }
 
