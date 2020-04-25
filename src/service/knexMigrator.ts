83c81d41b4d0cc81419636087d14ddc281ad1e81
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-04-25 20:42:40 +0545

    Use executeOperation function

--- src/service/knexMigrator.ts
+++ src/service/knexMigrator.ts
@@ -1,9 +1,9 @@
 import Knex from 'knex';
 
 import { dbLogger } from '../util/logger';
-import { getElapsedTime } from '../util/ts';
 import MigrationCommandContext from '../domain/MigrationCommandContext';
 import CommandResult from '../domain/CommandResult';
+import { executeOperation } from './execution';
 
 /**
  * A map of Knex's migration API functions.
@@ -27,44 +27,17 @@ export async function runMigrateFunc(
   context: MigrationCommandContext,
   func: (trx: Knex | Knex.Transaction, config: Knex.MigratorConfig) => Promise<any>
 ): Promise<CommandResult> {
-  const dbLog = dbLogger(context.connectionId);
-  const { connectionId, knexMigrationConfig } = context;
-  const funcName = func.name || 'func';
+  return executeOperation(context, async () => {
+    const { knexMigrationConfig } = context;
+    const funcName = func.name || 'func';
 
-  let error;
-  let data;
-
-  const timeStart = process.hrtime();
-
-  try {
+    const dbLog = dbLogger(context.connectionId);
     dbLog(`BEGIN: ${funcName}`);
-    data = await func(trx, knexMigrationConfig);
+    const data = await func(trx, knexMigrationConfig);
 
     dbLog(`END: ${funcName}`);
     dbLog('Result:\n%O', data);
-  } catch (e) {
-    dbLog(`Error caught for connection ${connectionId}:`, e);
-    error = e;
-  }
 
-  const timeElapsed = getElapsedTime(timeStart);
-
-  dbLog(`Execution completed in ${timeElapsed} s`);
-
-  const result: CommandResult = {
-    connectionId,
-    error,
-    data,
-    timeElapsed,
-    success: !error
-  };
-
-  // Invoke corresponding handlers if they're sent.
-  if (result.success && context.params.onSuccess) {
-    await context.params.onSuccess(result);
-  } else if (!result.success && context.params.onFailed) {
-    await context.params.onFailed(result);
-  }
-
-  return result;
+    return data;
+  });
 }
