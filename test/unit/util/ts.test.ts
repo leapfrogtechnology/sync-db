94574e98a1cb251218796f1971e43f2d2a7cf4b9
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-05-17 22:01:19 +0545

    Fix ts tests

--- test/unit/util/ts.test.ts
+++ test/unit/util/ts.test.ts
@@ -9,19 +9,20 @@ describe('UTIL: ts', () => {
       const result1 = ts.getTimestampString(new Date('December 17, 1995 03:24:00'));
       const result2 = ts.getTimestampString(new Date('December 17, 1995 15:24:00'));
 
-      expect(result1).to.equal('1995121732400');
-      expect(result2).to.equal('1995121732400');
+      expect(result1).to.equal('19951217032400');
+      expect(result2).to.equal('19951217032400');
     });
 
     it('should return a timestamp string with the current date if date is not provided.', () => {
+      const now = new Date();
       const result = ts.getTimestampString();
 
-      expect(result.startsWith(new Date().getFullYear().toString()));
-      expect(result.length).to.equal(13);
+      expect(result.startsWith(now.getFullYear().toString()));
+      expect(result.length).to.equal(14);
     });
 
     it('should return a value fully numeric.', () => {
-      expect(ts.getTimestampString()).to.match(/^\d+$/);
+      expect(ts.getTimestampString()).to.match(/^\d{14}$/);
     });
   });
 });
