bea4248745dd79ea24c6ce63e0b64d5472203bb7
Saugat Acharya <mesaugat@gmail.com>
2021-03-10 12:05:13 +0545

    Use -m twice for multiline commit message
    https://stackoverflow.com/a/5070502/2352587

--- release.sh
+++ release.sh
@@ -76,9 +76,7 @@ compare_and_release() {
     git config --global user.name "Travis CI"
 
     git add CHANGELOG.md
-    git commit -v --edit -m "${NEXT} Release :tada: :fireworks: :bell:
-    [skip ci]
-    "
+    git commit -v --edit -m "${NEXT} Release :tada: :fireworks: :bell:" -m "[skip ci]"
 
     git remote rm origin
     # Add new "origin" with access token in the git URL for authentication
