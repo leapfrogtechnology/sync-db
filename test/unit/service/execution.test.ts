6c84ebc9946f1c4cd666be9fd197b8212996331f
Sagar Chamling <sgr.raee@gmail.com>
2021-03-16 16:56:34 +0545

    Change deep memebers to include members

--- test/unit/service/execution.test.ts
+++ test/unit/service/execution.test.ts
@@ -46,8 +46,8 @@ describe('SERVICE: execution', () => {
         execution: 'parallel'
       } as Configuration);
 
-      expect(result).to.have.deep.members(['Task A', 'Task B', 'Task C', 'Task D']);
-      expect(tracker).to.have.deep.members(['Task C', 'Task D', 'Task A', 'Task B']);
+      expect(result).to.include.members(['Task A', 'Task B', 'Task C', 'Task D']);
+      expect(tracker).to.include.members(['Task C', 'Task D', 'Task A', 'Task B']);
     });
 
     it('should throw an error if unknown strategy is provided.', () => {
