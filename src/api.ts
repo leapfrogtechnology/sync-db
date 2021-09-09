710bf15a96fb3ac5987a94256e3e058af06f1777
Soneshaps <sonesh.aps@gmail.com>
2021-09-09 15:09:15 +0545

    LPD-3225 : Minor Fix

--- src/api.ts
+++ src/api.ts
@@ -52,7 +52,7 @@ export async function synchronize(
   const { knexMigrationConfig } = await init.prepare(config, {
     migrationPath,
     loadSqlSources: true,
-    loadMigrations: dirExist
+    loadMigrations: !params['skip-migration'] || dirExist
   });
 
   const connections = filterConnectionsAsRequired(mapToConnectionReferences(conn), params.only);
