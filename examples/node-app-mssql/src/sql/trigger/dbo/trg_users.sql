941a54e0a03be9b4fffce995bc1b94b347c3b57a
Saugat Acharya <mesaugat@gmail.com>
2022-12-20 20:59:32 +0545

    Update example to include trigger sample sql

--- examples/node-app-mssql/src/sql/trigger/dbo/trg_users.sql
+++ examples/node-app-mssql/src/sql/trigger/dbo/trg_users.sql
@@ -5,5 +5,5 @@ AS
   BEGIN
     DECLARE @deleted_record_count INT = (SELECT COUNT(*) FROM deleted);
 
-    PRINT CAST(@deleted_record_count AS VARCHAR(500)) + 'records deleted.';
+    PRINT CAST(@deleted_record_count AS VARCHAR(500)) + ' user records deleted.';
   END;
