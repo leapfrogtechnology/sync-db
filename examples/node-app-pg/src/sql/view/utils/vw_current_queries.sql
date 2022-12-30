14d40c6c871930269d8083e8ff29d514c9e14249
Samir Oli <samiroli789@gmail.com>
2022-12-30 12:45:10 +0545

    Add more examples for `.drop` terminator

---
+++ examples/node-app-pg/src/sql/view/utils/vw_current_queries.sql
@@ -0,0 +1,9 @@
+--
+-- View to fetch the list of current queries executed by users who are connected to the database
+-- This view is written to display the purpose of `.drop` terminator in the config file. Since, in the config file,
+-- `.drop` is appended to this database object's name, sync-db will only attempt to drop it and never attempt to sync it.
+--
+CREATE VIEW utils.vw_current_queries
+AS (
+  SELECT datname, usename, query FROM pg_stat_activity;
+);
