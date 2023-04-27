ed2aab88b02b4e0ecc772e30cead82acbd201825
kshitish182 <kshitish182@gmail.com>
2023-04-27 12:06:40 +0545

    Run prettify

--- src/config.ts
+++ src/config.ts
@@ -58,7 +58,10 @@ export async function loadConfig(configFilename: string = CONFIG_FILENAME): Prom
 
   log('Resolved config file.');
 
-  const loaded = mergeDeepRight<Configuration, Configuration>(DEFAULT_CONFIG, loadedConfig) as unknown as Configuration;
+  const loaded = (mergeDeepRight<Configuration, Configuration>(
+    DEFAULT_CONFIG,
+    loadedConfig
+  ) as unknown) as Configuration;
 
   validate(loaded);
 
